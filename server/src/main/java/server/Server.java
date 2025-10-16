b233394ec66549b42a6a5b8f29d5bd99acd0e045
Kaylie <kayliereinhart@gmail.com>
2025-10-16 12:52:21 -0600

    Started working on register endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,24 +1,36 @@
 package server;
 
+import com.google.gson.Gson;
+import datamodel.User;
 import io.javalin.*;
+import io.javalin.http.Context;
 
 public class Server {
 
-    private final Javalin javalin;
+    private final Javalin server;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        server = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        server.post("user", this::register);
 
+    }
+
+    private void register(Context ctx) {
+        Gson serializer = new Gson();
+        String reqJson = ctx.body();
+        var req = serializer.fromJson(reqJson, User.class);
+
+        var res = userService.register(req);
     }
 
     public int run(int desiredPort) {
-        javalin.start(desiredPort);
-        return javalin.port();
+        server.start(desiredPort);
+        return server.port();
     }
 
     public void stop() {
-        javalin.stop();
+        server.stop();
     }
 }
