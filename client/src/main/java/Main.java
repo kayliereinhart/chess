85cf8101a374ca11172bc85108e833f04beb9303
Kaylie <kayliereinhart@gmail.com>
2025-11-14 16:03:15 -0700

    Made ServerFacade and Client

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,7 +2,17 @@ import chess.*;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        String serverUrl = "http://localhost:8080";
+//        if (args.length == 1) {
+//            serverUrl = args[0];
+//        }
+
+        try {
+            new Client(serverUrl).run();
+
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
+        //System.out.println("♕ 240 Chess Client: ");
     }
 }
---
+++ shared/src/main/java/server/ServerFacade.java
@@ -0,0 +1,6 @@
+package server;
+
+public class ServerFacade {
+
+    //public AuthData register(UserData request) {}
+}
