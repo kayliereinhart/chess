14d9f7442dad3ac829d783f8976a2d3517912dd1
Kaylie <kayliereinhart@gmail.com>
2025-12-09 14:03:19 -0700

    Fixed unnamed variable

--- shared/src/main/java/gsonbuilder/GameGsonBuilder.java
+++ shared/src/main/java/gsonbuilder/GameGsonBuilder.java
@@ -30,7 +30,7 @@ public class GameGsonBuilder {
         });
 
         gsonBuilder.registerTypeAdapter(MovementRule.class,
-                (JsonDeserializer<MovementRule>) (el, _, ctx) -> {
+                (JsonDeserializer<MovementRule>) (el, type, ctx) -> {
             if (!el.isJsonObject()) {
                 return null;
             }
